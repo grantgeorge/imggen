fdd22a0d3cb8c8ff20b67d6b651cdd3537434f20
grant <grantelliottgeorge@gmail.com>
2013-07-25 14:59:22 -0400

    Testing news tutorial

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,10 +1,1 @@
-<?php
-// Create connection
-$con=mysqli_connect("localhost","root","grantD3v","imggen_db");
-
-// Check connection
-if (mysqli_connect_errno($con))
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-?>
+<p>"Hello, world!"</p>
