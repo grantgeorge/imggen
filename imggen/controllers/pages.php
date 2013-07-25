e265d85d3c06d56331198d2b66e61b32338d0f6a
grant <grantelliottgeorge@gmail.com>
2013-07-24 22:07:37 -0400

    another test

--- imggen/controllers/pages.php
+++ imggen/controllers/pages.php
@@ -5,7 +5,7 @@ class Pages extends CI_Controller {
 	public function view($page = 'home')
 	{
 
-		if ( ! file_exists('application/views/pages/'.$page.'.php'))
+		if ( ! file_exists('imggen/views/pages/'.$page.'.php'))
 		{
 			// Whoops, we don't have a page for that!
 			show_404();
