de83fa5ec58ada5477559d96f221efcfbb48828b
grant <grantelliottgeorge@gmail.com>
2013-07-25 15:01:33 -0400

    Testing news tutorial

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,1 +1,13 @@
-<p>"Hello, world!"</p>
+<?php
+// Create connection
+$con=mysqli_connect("example.com","peter","abc123","my_db");
+
+// Check connection
+if (mysqli_connect_errno($con))
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  else{
+  	echo "Connectin established";
+  }
+?>
