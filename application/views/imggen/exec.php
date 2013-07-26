b1c97729c0a3d5ba65a6cc73aa64b61e6c92e4ab
grantgeorge <grantelliottgeorge@gmail.com>
2013-07-26 11:46:45 -0400

    testing

--- application/views/imggen/exec.php
+++ application/views/imggen/exec.php
@@ -4,4 +4,5 @@ shell_exec('cd /var/www/imggen/application/static/js/ && phantomjs image_capture
 $result = shell_exec('phantomjs --version');
 echo $result;
 ?>
+<img src="http://192.241.148.248/imggen/application/image_captures/TestScript.png" alt="Failed to load">
 <h1><a href="http://192.241.148.248/imggen/index.php/imggen">Index</a></h1>
