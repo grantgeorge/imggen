afae2140374a5a77793df33d7e06a845618807c3
grantgeorge <grantelliottgeorge@gmail.com>
2013-07-26 11:17:38 -0400

    testing

--- application/static/js/image_capture.js
+++ application/static/js/image_capture.js
@@ -1,9 +1,9 @@
 var page = require('webpage').create();
-page.clipRect = { top: 0, left: 0, width: 450, height: 250 };
 page.open('http://192.241.148.248/imggen/index.php/imggen/Tag1', function () {
 	var title = page.evaluate(function() {
 		return document.title;
 	});
+	page.clipRect = { top: 0, left: 0, width: 450, height: 250 };
     page.render("../../image_captures/"+title+".png");
     phantom.exit();
 });
