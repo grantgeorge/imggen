1fbff98cf422533904b2c6b1432efc33316d2fde
grantgeorge <grantelliottgeorge@gmail.com>
2013-07-26 11:41:25 -0400

    testing

--- application/views/imggen/exec.php
+++ application/views/imggen/exec.php
@@ -1,7 +1,6 @@
 <h2>exec</h2>
 <?php
-shell_exec('cd /var/www/imggen/application/static/js/');
-shell_exec('phantomjs image_capture.js');
+shell_exec('cd /var/www/imggen/application/static/js/ && phantomjs image_capture.js');
 $result = shell_exec('phantomjs --version');
 echo $result;
 ?>
