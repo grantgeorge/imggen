8f738b70bbdbcea4f56d58acd611df55b462e6d0
grant <grantelliottgeorge@gmail.com>
2013-07-26 03:10:27 -0400

    phantomjs

--- application/static/js/image_capture.js
+++ application/static/js/image_capture.js
@@ -1,6 +1,6 @@
 var page = require('webpage').create();
 page.clipRect = { top: 0, left: 0, width: 450, height: 250 };
-page.open('http://192.241.148.248/imggen/index.php/imggen/TestInput', function () {
+page.open('http://192.241.148.248/imggen/index.php/imggen/Tag1', function () {
 	var title = page.evaluate(function() {
 		return document.title;
 	});
