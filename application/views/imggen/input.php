334b01f5be9813149d6b2183cdce3a9f669c3c5f
grant <grantelliottgeorge@gmail.com>
2013-07-25 20:40:46 -0400

    fix error

--- application/views/imggen/input.php
+++ application/views/imggen/input.php
@@ -4,6 +4,7 @@
 
 <?php echo form_open('imggen/input') ?>
 
+	<label for="text">Text</label>
 	<textarea name="text"></textarea><br />
 
 	<input type="submit" name="submit" value="Create news item" />
