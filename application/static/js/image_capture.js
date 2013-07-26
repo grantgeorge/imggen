3d18a929cf6fb92d72e49ca20b19b4db194f989e
grant <grantelliottgeorge@gmail.com>
2013-07-26 02:42:00 -0400

    js

--- application/static/js/image_capture.js
+++ application/static/js/image_capture.js
@@ -1,13 +1,9 @@
 var page = require('webpage').create();
-page.clipRect = { top: 0, left: 0, width: 450, height: 250 };
 page.open('http://192.241.148.248/imggen/index.php/imggen/TestInput', function () {
-<<<<<<< HEAD
 	var title = page.evaluate(function() {
 		return document.title;
 	});
+	page.clipRect = { top: 0, left: 0, width: 450, height: 250 };
     page.render("../../image_captures/"+title+".png");
-=======
-    page.render('../../image_captures/Test3.png');
->>>>>>> 5a6cd5d6cf36073cfb66f6fe07469616b1775d03
     phantom.exit();
 });
