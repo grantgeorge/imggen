8ec0a85cf4694839e92e6dd4829c24063a56ecb6
grantgeorge <grantelliottgeorge@gmail.com>
2013-07-26 13:40:25 -0400

    adding exec page

--- application/views/imggen/exec.php
+++ application/views/imggen/exec.php
@@ -1,8 +1,8 @@
 <h2>exec</h2>
 <?php
-shell_exec('cd /var/www/imggen/application/static/js/ && phantomjs image_capture.js');
-echo'<img src="http://192.241.148.248/imggen/application/image_captures/'.$imggen_item['title'].'.png" alt="Failed to load">'
-echo'<h1><a href="http://192.241.148.248/imggen/index.php/imggen">Index</a></h1>'
+#shell_exec('cd /var/www/imggen/application/static/js/ && phantomjs image_capture.js');
+#echo'<img src="http://192.241.148.248/imggen/application/image_captures/'.$imggen_item['title'].'.png" alt="Failed to load">'
+#echo'<h1><a href="http://192.241.148.248/imggen/index.php/imggen">Index</a></h1>'
 
 #<?php
 # echo '<h2>'.$imggen_item['title'].'</h2>';
