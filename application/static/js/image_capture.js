011a178ed30b46fb6ef197d365f0b61ed84b160f
grantgeorge <grantelliottgeorge@gmail.com>
2013-07-26 14:47:59 -0400

    script

--- application/static/js/image_capture.js
+++ application/static/js/image_capture.js
@@ -1,9 +1,24 @@
-var page = require('webpage').create();
-page.open('http://192.241.148.248/imggen/index.php/imggen/TestScript', function () {
-	var title = page.evaluate(function() {
-		return document.title;
+var page = require('webpage').create(),
+	system = require('system');
+
+if (system.args.length === 1) {
+	console.log('Pass arg when invoking script');
+	phantom.exit();
+}
+else{
+	title = system.args[1];
+	console.log(title);
+
+	page.open('http://192.241.148.248/imggen/index.php/imggen/'+title, function ( status ) {
+		if (status !== 'success') {
+	        console.log('Unable to load the address!');
+	        phantom.exit();
+    } else {
+	        window.setTimeout(function () {
+	        	page.clipRect = { top: 0, left: 0, width: 450, height: 250 };
+		        page.render("../../image_captures/"+title+".png");
+		        phantom.exit();
+	        }, 200);
+    	}
 	});
-	page.clipRect = { top: 0, left: 0, width: 450, height: 250 };
-    page.render("../../image_captures/"+title+".png");
-    phantom.exit();
-});
+}
