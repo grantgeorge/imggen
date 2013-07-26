51ca97d236ac942c01321405b1049aa83b285245
grant <grantelliottgeorge@gmail.com>
2013-07-26 03:02:13 -0400

    yurp

--- application/static/js/image_capture.js
+++ application/static/js/image_capture.js
@@ -4,6 +4,6 @@ page.open('http://192.241.148.248/imggen/index.php/imggen/TestInput', function (
 	var title = page.evaluate(function() {
 		return document.title;
 	});
-    page.render(title+".png");
+    page.render("../../image_captures/"+title+".png");
     phantom.exit();
 });
