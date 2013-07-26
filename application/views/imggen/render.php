ade8b1e85bdafa5bbd2b4fc162b6175839f8d6b2
grantgeorge <grantelliottgeorge@gmail.com>
2013-07-26 13:57:13 -0400

    render image

--- application/views/imggen/render.php
+++ application/views/imggen/render.php
@@ -2,7 +2,7 @@
 #echo '<h2>Render page</h2>';
 echo '<h2>'.$imggen_item['title'].'</h2>';
 #shell_exec('cd /var/www/imggen/application/static/js/ && phantomjs image_capture.js');
-echo'<img src="http://192.241.148.248/imggen/application/image_captures/'.$imggen_item['title'].'.png" alt="Failed to load">'
+echo'<img src="http://192.241.148.248/imggen/application/image_captures/'.$imggen_item['title'].'.png" alt="Failed to load">';
 #echo'<h1><a href="http://192.241.148.248/imggen/index.php/imggen">Index</a></h1>'
 
 #echo '<h2>'.$imggen_item['title'].'</h2>';
