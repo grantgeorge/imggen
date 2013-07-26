59695a4eab4c12b2caa1cd8e2250dda63945ecb2
grant <grantelliottgeorge@gmail.com>
2013-07-25 21:23:56 -0400

    fix error

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,8 +53,10 @@ $route['(:any)'] = 'imggen/view/$1';
 
 */
 
-$route['(:any)'] = 'imggen/view/$1';
-$route['default_controller'] = 'imggen/view';
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
+$route['(:any)'] = 'pages/view/$1';
+$route['default_controller'] = 'pages/view';
 
 /*
 
