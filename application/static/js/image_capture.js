4b91fbb7ee5f20087cd2c9c5c82f23620cea0e8b
root <root@grantdev.(none)>
2013-07-26 06:13:34 +0000

    fix

--- application/static/js/image_capture.js
+++ application/static/js/image_capture.js
@@ -1,10 +1,6 @@
 var page = require('webpage').create();
 page.clipRect = { top: 0, left: 0, width: 450, height: 250 };
 page.open('http://192.241.148.248/imggen/index.php/imggen/TestInput', function () {
-<<<<<<< HEAD
-    page.render('../../image_captures/test2.png');
-=======
     page.render('../../image_captures/Test4.png');
->>>>>>> 84cda492087a7c6cc2359648871a219841347896
     phantom.exit();
 });
