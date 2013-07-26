4fc1d77f7ec37d7ac59a1eb220ed4139074cb935
grant <grantelliottgeorge@gmail.com>
2013-07-26 00:52:35 -0400

    add css

--- application/views/imggen/view.php
+++ application/views/imggen/view.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="views/templates/css/image_view.css" type="text/css">
+<link rel="stylesheet" href="css/image_view.css" type="text/css">
 <?php
 # echo '<h2>'.$imggen_item['title'].'</h2>';
 echo $imggen_item['text'];
