b5ccc4d4a2551f10674d61983816365f19950c48
grant <grantelliottgeorge@gmail.com>
2013-07-26 02:26:08 -0400

    adding phantomjs

--- application/views/imggen/exec.php
+++ application/views/imggen/exec.php
@@ -1,5 +1,5 @@
 <h2>exec</h2>
 <?php
-$result = shell_exec('phantomjs --version');
+$result = shell_exec('phantomjs http://192.241.148.248/imggen/application/static/js/image_capture.js');
 echo $result;
 ?>
