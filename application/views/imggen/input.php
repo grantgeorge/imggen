84a4fa5f794a06bb00691aa4d84517bdf33a87fe
grant <grantelliottgeorge@gmail.com>
2013-07-25 20:40:14 -0400

    fix error

--- application/views/imggen/input.php
+++ application/views/imggen/input.php
@@ -1,9 +1,11 @@
-<h2>Create an image</h2>
+<h2>Input HTML</h2>
 
 <?php echo validation_errors(); ?>
 
 <?php echo form_open('imggen/input') ?>
 
-	
+	<textarea name="text"></textarea><br />
+
+	<input type="submit" name="submit" value="Create news item" />
 
 </form>
