f651ed0f3d9f27dff3bc9917b87732cdb88a019a
grant <grantelliottgeorge@gmail.com>
2013-07-26 02:28:14 -0400

    adding phantomjs

--- application/static/js/image_capture.js
+++ application/static/js/image_capture.js
@@ -1,6 +1,6 @@
 var page = require('webpage').create();
 page.clipRect = { top: 0, left: 0, width: 450, height: 250 };
 page.open('http://192.241.148.248/imggen/index.php/imggen/TestInput', function () {
-    page.render('../../image_captures/Test4.png');
+    page.render('../../image_captures/Test5.png');
     phantom.exit();
 });
